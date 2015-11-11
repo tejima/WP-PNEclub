067f60b29d09c0dbba182ad30937c0d67d35cb4f
手嶋守 <tejima@gmail.com>
2015-11-11 14:56:01 +0900

    初期リリース

---
+++ README.md
@@ -0,0 +1,84 @@
+# WP-PNEclub使い方
+
+## イントロダクション
+
+WP-PNEclubは、PNE.clubの集金アカウント情報を元に、
+有料ユーザーのみの限定コンテンツをWordPress内に表示するプラグインです。
+
+有料／無料の細かいWordPress内の表示コントロールには、既存プラグインであるWP-Membersプラグインを利用しています。
+PNE.club及びWP-PNEclubは、ユーザーの会員登録、カード集金／督促、大会、パスワード再発行などを担当します。
+
+WP-PNEclubはWordPress内に、ログインボックスを表示し、PNE.clubとのアカウント認証を行う
+該当のクラブの正しい利用者であることが確認されれば、WordPressの特定のユーザーとしてログインを継続します。
+
+## 事前準備
+
+* WP-Membersプラグインが動作するバージョンのWordPressのインストール
+* PNE.club (http://pne.club)でのクラブ加盟
+* 自社のクラブIDの取得（PNE.club管理画面=>表示設定）
+* （オプション）コースIDの取得（PNE.club管理画面=>コース設定）
+* WordPress内に、プレミアム読者としてログインさせるユーザーの追加
+ * 権限は必ず「購読者」、名前等は自由
+
+## インストール
+
+* WP-Membersプラグインのインストール https://ja.wordpress.org/plugins/wp-members/
+* WP-PNEclubプラグインのインストール ベータ中につき非公開
+
+## WP-PNEclub設定
+
+* pneclub.php内の設定値を以下を参考にして変更する
+
+```
+add_option('pneclub_wordpresshost', 'http://blog.s1.cqc.jp/', null, 'yes');
+※WordPressサイトのURL
+
+add_option('pneclub_servicename', 'プレミアムプレス', null, 'yes');
+※WordPressサイトの名称
+
+add_option('pneclub_clubid', 'yx4ugVP6HA', null, 'yes');
+※PNE.clubで加盟したクラブID
+
+add_option('pneclub_targetcourse', 'ANY', null, 'yes');
+※有料ユーザーとして認めるコースID（通常すべてを受け入れる”ANY”のままで良い）
+
+add_option('pneclub_readerlogin', 'reader', null, 'yes');
+add_option('pneclub_readerpassword', 'gatagatamichi', null, 'yes');
+※プレミアム読者のユーザーアカウント
+
+add_option('pneclub_refreshtime', 3, null, 'yes');
+※ログイン後のリダイレクト待ち時間
+```
+
+## WP-Members設定
+
+* ダイアログ入力値の設定
+
+WP-Members設定＝＞ダイアログ
+＝＞アクセス制限された投稿記事および固定ページ、ログインおよび登録フォームの上に表示
+
+を以下のタグに変更する。
+
+```
+オンラインサービスをご利用いただくには会員認証が必要です。
+
+<form action="/wp-admin/admin-post.php" method="post">
+<label>e-mail</label>
+<input type="hidden" name="action" value="pnelogin">
+<input type="email" name="email"><br>
+<label>password</label>
+<input type="password" name="password"><br>
+<input type="submit" value="ログイン">
+</form>
+<br>
+
+※まだ会員登録がお済みでない方は<a href="/premium">会員登録</a>にお進みください。
+```
+
+* 公開範囲等の設定
+
+クラブの運営スタイルに合わせて適宜変更する。
+
+参考設定値
+* http://p.pne.jp/d/201511111446.png
+* http://p.pne.jp/d/201511111447.png
---
+++ pneclub.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+Plugin Name: PNE.club
+Plugin URI: http://pne.club
+Description: PNE.club premium user control for WordPress
+Version: 1.0
+Author: Mamoru Tejima
+Author URI: http://twitter.com/tejima
+License: Apache2.0
+*/
+
+add_option('pneclub_wordpresshost', 'http://blog.s1.cqc.jp/', null, 'yes');
+add_option('pneclub_servicename', 'プレミアムプレス', null, 'yes');
+add_option('pneclub_clubid', 'yx4ugVP6HA', null, 'yes');
+add_option('pneclub_targetcourse', 'ANY', null, 'yes');
+add_option('pneclub_readerlogin', 'reader', null, 'yes');
+add_option('pneclub_readerpassword', 'gatagatamichi', null, 'yes');
+add_option('pneclub_refreshtime', 3, null, 'yes');
+
+add_option('pneclub_apihost', 'https://www.pne.club/', null, 'yes');
+
+add_action('admin_post_nopriv_pnelogin', 'function_pnelogin');
+add_action('admin_post_pnelogin', 'function_pnelogin');
+
+function function_pnelogin()
+{
+    $url = 'https://www.pne.club/auth';
+    $data = array(
+        'email' => $_REQUEST['email'],
+        'password' => $_REQUEST['password'],
+        'club_id' => get_option('pneclub_clubid'),
+        'course' => get_option('pneclub_targetcourse'),
+    );
+    $options = array('http' => array(
+        'method' => 'POST',
+        'content' => http_build_query($data),
+    ));
+
+    $contents_json = file_get_contents($url, false, stream_context_create($options));
+    $contents = json_decode($contents_json, true);
+
+    if(!isset($contents)){
+        $contents = array('result' == '500');
+    }
+
+    switch ($contents['result']) {
+        case 200:
+        case '200':
+            $creds = array();
+            $creds['user_login'] = get_option('pneclub_readerlogin');
+            $creds['user_password'] = get_option('pneclub_readerpassword');
+            $creds['remember'] = true;
+            $user = wp_signon($creds, false);
+            $state = 'SUCCESS';
+            break;
+
+        case '401':
+        case '402':
+        case '403':
+        case '500':
+        default:
+            $state = 'ERROR';
+            break;
+    }
+
+    $wordpresshost = get_option('pneclub_wordpresshost');
+    $servicename = get_option('pneclub_servicename');
+    $refreshtime = get_option('pneclub_refreshtime');
+
+    if ($state == 'SUCCESS') {
+        $result = <<<EOF
+<HTML>
+<HEAD>
+<META http-equiv="refresh" content="{$refreshtime}; url={$wordpresshost}">
+<TITLE></TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+ログインしました。
+{$refreshtime}秒で自動的にサイトが移動しない場合は、こちらをクリックして進んでください。
+<a href="{$wordpresshost}">{$servicename}</a>
+
+</BODY>
+</HTML>
+EOF;
+    } elseif ($state == 'ERROR') {
+        $result = <<<EOF
+<HTML>
+<HEAD>
+<META http-equiv="refresh" content="{$refreshtime}; url={$wordpresshost}">
+<TITLE></TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+ログインに失敗しました。
+<a href="{$wordpresshost}">{$servicename}</a>
+
+</BODY>
+</HTML>
+EOF;
+    }
+    status_header(200);
+    die($result);
+}
